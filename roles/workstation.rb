a598d772814350c8e7907be607bd1634873f8478
Hippie Hacker <chris@hippiehacker.org>
2013-02-05 07:29:24 -0800

    opscode user and pass on workstation

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -4,3 +4,11 @@ run_list(
   "role[base]",
   "recipe[ii-knife-workstation]"
 )
+default_attributes(
+  workstation: {
+    username: 'opscode',
+    password: 'opscode'
+    # if you want to increase size from default of 1024x768
+    # geometry: '1280x1024' 
+  }
+)
