8dafb359ba7b3b2991e6a38eada81a9fb060aed7
Hippie Hacker <chris@hippiehacker.org>
2013-02-04 18:30:46 -0800

    rename_of_fileserver_cb

--- roles/fileserver.rb
+++ roles/fileserver.rb
@@ -1,7 +1,7 @@
 name "fileserver"
 description "Fileserver for Chef installer packages, Sublime Text 2 and more"
 
-run_list "recipe[fileserver]"#, "recipe[debmirror::precise]"
+run_list "recipe[ii-fileserver]"#, "recipe[debmirror::precise]"
 
 default_attributes(
   "fileserver" => {
