f1e44a65979acc7d76ac33767f51eea40f7f897f
Hippie Hacker <chris@hippiehacker.org>
2013-02-04 21:21:12 -0800

    foo

--- roles/fog-lab.rb
+++ roles/fog-lab.rb
@@ -2,7 +2,6 @@ name "fog-lab"
 description "The machine that will run virtualization of everything else"
 
 run_list(
-  "role[base]",
   "recipe[apt]",
   "recipe[apt::cacher-ng]",
   "recipe[ntp]",
