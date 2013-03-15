225ab8d2ceb730b38b7288f4ea0e79c757a533f1
aseresun <aaron.seresun@nordstrom.com>
2013-03-14 20:07:09 -0700

    Fix resolution text

--- .chef/plugins/knife/source_ingredient_chef_client.rb
+++ .chef/plugins/knife/source_ingredient_chef_client.rb
@@ -49,7 +49,7 @@ module KnifePlugins
         FileUtils.touch(File.join(Chef::Config[:data_bag_path],'.writeable'))
         FileUtils.touch(File.join(Chef::Config[:file_cache_path],'.writeable'))
       rescue
-        puts "Set role_path and file_cache_path in knife.rb to somewhere writable"
+        puts "Set data_bag_path and file_cache_path in knife.rb to somewhere writable"
         exit 1
       end
       data_bag_name = 'chef' # maybe set this via cmdline later
